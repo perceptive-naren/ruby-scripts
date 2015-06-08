802ebc667771dbe6bdff8d3d95932b7e5a94e785
dhyan <dhyanbaba@gmail.com>
2015-06-08 19:05:50 +0530

    ruby script to test Tmbedr services

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source "https://rubygems.org"
+gem 'tmbedr',path: '~/work/tmbedr'
+gem 'dotenv'
---
+++ test.rb
@@ -0,0 +1,26 @@
+# Load all the required libraries
+require 'tmbedr'
+require 'json'
+require 'dotenv'
+Dotenv.load
+
+class Test
+  EMBEDR_HASH=
+      {
+          "content_provider" => "twistage",
+          "iframe" => "0",
+          "action" => "oembed",
+          "controller" => "oembed",
+          "frameborder" => 0,
+          "url" => ENV['TMBEDR_VIDEO_URL'],
+          "viewer_profile" => ENV['VIEWER_PROFILE'],
+          "tenant" => ENV['TENANT_SLUG'],
+          "embedr_url" => ENV['EMBEDR_API_HOST'],
+          "test" => true,
+
+      }
+end
+test = Tmbedr.get_embed_code(Test::EMBEDR_HASH)
+puts test
+
+
